67578eeb88c05e62fe5d8dff602726d23b82a5ef
Marko Blazevic <www.markoblazevic@gmail.com>
2020-12-04 21:08:18 -0300

    added Dropdown in navbar

--- my-script.js
+++ my-script.js
@@ -4,10 +4,16 @@ $(document).ready(function () {
   });
 });
 
-(function () {
+$(document).ready(function () {
   if (document.location.hash) {
     setTimeout(function () {
       window.scrollTo(window.scrollX, window.scrollY - 66);
     }, 10);
   }
-})();
+});
+
+$('.click-offset').click(function () {
+  setTimeout(function () {
+    window.scrollTo(window.scrollX, window.scrollY - 66);
+  }, 10);
+});
