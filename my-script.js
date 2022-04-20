1f74576bdc0facc14974cd3f81cc7b92a0e2358b
Marko Blazevic <www.markoblazevic@gmail.com>
2022-04-20 15:22:49 -0300

    Laptop baterija je gotova. Prelazim na drugi komp

--- my-script.js
+++ my-script.js
@@ -1,16 +1,12 @@
-$(document).ready(function () {
-  $('table tr').click(function () {
-    window.location = $(this).data('href');
-  });
+$('table tr').click(function () {
+  window.location = $(this).data('href');
 });
 
-$(document).ready(function () {
-  if (document.location.hash) {
-    setTimeout(function () {
-      window.scrollTo(window.scrollX, window.scrollY - 66);
-    }, 10);
-  }
-});
+if (document.location.hash) {
+  setTimeout(function () {
+    window.scrollTo(window.scrollX, window.scrollY - 66);
+  }, 10);
+}
 
 $('.click-offset').click(function () {
   setTimeout(function () {
