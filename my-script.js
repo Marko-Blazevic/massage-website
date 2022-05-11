354571850c2c75c31c6059741366eb2e0d025b39
Marko Blazevic <www.markoblazevic@gmail.com>
2022-05-11 15:33:29 -0300

    Added animations to massages. Changed Index, About animations to GSAP.

--- my-script.js
+++ my-script.js
@@ -2,24 +2,50 @@ $('table tr').click(function () {
   window.location = $(this).data('href');
 });
 
+// href when clicked on table row
 if (document.location.hash) {
   setTimeout(function () {
     window.scrollTo(window.scrollX, window.scrollY - 66);
   }, 10);
 }
 
+// href when clicked on nav dropdow Usluge
 $('.click-offset').click(function () {
   setTimeout(function () {
-    window.scrollTo(window.scrollX, window.scrollY - 66);
+    window.scrollTo(window.scrollX, window.scrollY - 75);
   }, 10);
 });
 
-const masaze = gsap.utils.toArray('.slide-up');
 gsap.registerPlugin(ScrollTrigger);
-masaze.forEach((masaza) => {
-  gsap.from(masaza, {
-    scrollTrigger: masaza,
-    y: 100,
+
+const slideUp = gsap.utils.toArray('.slide-up');
+slideUp.forEach((elem) => {
+  gsap.from(elem, {
+    scrollTrigger: elem,
+    y: 200,
     duration: 1,
+    opacity: 0.2,
   });
 });
+
+const slideLeft = gsap.utils.toArray('.slide-left');
+slideLeft.forEach((elem) => {
+  gsap.from(elem, {
+    scrollTrigger: elem,
+    x: -200,
+    duration: 1,
+    opacity: 0.2,
+  });
+});
+
+const slideRight = gsap.utils.toArray('.slide-right');
+slideRight.forEach((elem) => {
+  gsap.from(elem, {
+    scrollTrigger: elem,
+    x: 200,
+    duration: 1,
+    opacity: 0.2,
+  });
+});
+
+gsap.to('.logo', { rotation: 360, duration: 1, delay: 1 });
