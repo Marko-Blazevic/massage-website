72ae00cb237cd49c3d56ce5ecf72757d27af3203
Marko Blazevic <www.markoblazevic@gmail.com>
2022-07-04 18:28:21 -0300

    Auto generated previous, current and next month dates

--- calendarscript.js
+++ calendarscript.js
@@ -1,5 +1,5 @@
 const date = new Date();
-
+date.setMonth(7);
 const days = document.querySelector('.days');
 const currentMonth = document.querySelector('.date h1');
 const currentDate = document.querySelector('.date p');
@@ -21,21 +21,58 @@ const meseci = [
   'Decembar',
 ];
 const dani = [
+  'Nedelja',
   'Ponedeljak',
   'Utorak',
   'Sreda',
   'Četvrtak',
   'Petak',
   'Subota',
-  'Nedelja',
 ];
 
 const mesec = meseci[month];
 const dan = dani[day];
-currentMonth.innerHTML = mesec;
-
+currentMonth.innerHTML = mesec.toUpperCase();
 currentDate.innerHTML = `${dan} ${date.getDate()} ${mesec} ${date.getFullYear()}`;
 
-for (i = 1; i <= 31; i++) {
+const lastDateOfMonth = new Date(
+  date.getFullYear(),
+  date.getMonth() + 1,
+  0
+).getDate();
+const prevMonthLastDate = new Date(
+  date.getFullYear(),
+  date.getMonth(),
+  0
+).getDate();
+const prevMonthLastDayIndex = new Date(
+  date.getFullYear(),
+  date.getMonth(),
+  0
+).getDay();
+const firstDateNextMonth = new Date(
+  date.getFullYear(),
+  date.getMonth() + 1,
+  1
+).getDate();
+const nextMonthFirstDayIndex = new Date(
+  date.getFullYear(),
+  date.getMonth() + 1,
+  1
+).getDay();
+
+console.log(prevMonthLastDate);
+console.log(prevMonthLastDayIndex);
+console.log(lastDateOfMonth);
+console.log(nextMonthFirstDayIndex);
+console.log(date);
+
+for (j = prevMonthLastDayIndex; j > 0; j--) {
+  days.innerHTML += `<div class="prev-date">${prevMonthLastDate - j + 1}</div>`;
+}
+for (i = 1; i <= lastDateOfMonth; i++) {
   days.innerHTML += `<div>${i}</div>`;
+}
+for (x = 1; x < 9 - nextMonthFirstDayIndex; x++) {
+  days.innerHTML += `<div class="next-date">${x}</div>`;
 }
