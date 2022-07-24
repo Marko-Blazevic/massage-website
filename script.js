d41b8b179700c2a8d213e50f321967a8537d4497
Marko Blazevic <www.markoblazevic@gmail.com>
2022-07-24 17:32:32 -0300

    Added screen orientation @media for index-image.

---
+++ script.js
@@ -0,0 +1,12 @@
+// const indexImage = document.querySelector('.index-img');
+
+// var w = window.innerWidth;
+// var h = window.innerHeight;
+
+// if (h / w < 1) {
+//   indexImage.style.width = '50%';
+// } else {
+//   indexImage.style.width = '100%';
+// }
+
+// console.log(w, h);
