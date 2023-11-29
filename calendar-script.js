6bda23f06deba9ef8116cc0b30312edb61243244
Marko Blazevic <www.markoblazevic@gmail.com>
2023-11-29 17:42:06 -0300

    Fixed calendar not changing on arrow click. Removed code from renderCalendar() to outside scope that is not directly connected to calendar render.

--- calendar-script.js
+++ calendar-script.js
@@ -16,11 +16,37 @@ let checkTime;
 let massageDataValue;
 let timeDataIndex;
 
-async function fetchScheduleData() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-  const scheduleData = await response.json();
-  return scheduleData;
-}
+// async function fetchScheduleData() {
+//   const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+//   const scheduleData = await response.json();
+//   return scheduleData;
+// }
+
+//this data have to come from database
+const scheduleValuesData = [
+  {
+    date: '20231030',
+    time: [
+      2, 3, 4, 5, 6, 7, 8, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+      25, 26, 27, 28, 29, 30, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45,
+      46, 47,
+    ],
+  },
+  {
+    date: '20231028',
+    time: [
+      2, 3, 4, 7, 8, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+      27, 28, 29, 30, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43,
+    ],
+  },
+  {
+    date: '20231029',
+    time: [
+      1, 2, 3, 4, 5, 6, 7, 8, 12, 13, 14, 15, 25, 26, 27, 28, 29, 30, 34, 35,
+      36, 37, 38, 39, 40, 41, 42, 43, 44, 45,
+    ],
+  },
+];
 
 let calendarModal = new bootstrap.Modal(
   document.getElementById('calendar-modal'),
@@ -183,32 +209,6 @@ const renderCalendar = () => {
     calendarErrorModal.show();
   };
 
-  //this data have to come from database
-  const scheduleValuesData = [
-    {
-      date: '20231030',
-      time: [
-        2, 3, 4, 5, 6, 7, 8, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
-        25, 26, 27, 28, 29, 30, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45,
-        46, 47,
-      ],
-    },
-    {
-      date: '20231028',
-      time: [
-        2, 3, 4, 7, 8, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
-        26, 27, 28, 29, 30, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43,
-      ],
-    },
-    {
-      date: '20231029',
-      time: [
-        1, 2, 3, 4, 5, 6, 7, 8, 12, 13, 14, 15, 25, 26, 27, 28, 29, 30, 34, 35,
-        36, 37, 38, 39, 40, 41, 42, 43, 44, 45,
-      ],
-    },
-  ];
-
   //for "const date" - month and year are changing because of date manipulation during month change, but the date for date is not i.e. it is today's date
   const checkClickedDate = (elem) => {
     const currentDate = new Date();
@@ -247,152 +247,159 @@ const renderCalendar = () => {
       timeCheck();
     });
   });
-  arrowPrev.addEventListener('click', () => {
-    date.setMonth(date.getMonth() - 1);
-    renderCalendar();
-  });
-  arrowNext.addEventListener('click', () => {
-    date.setMonth(date.getMonth() + 1);
-    renderCalendar();
-  });
+};
 
-  //setting time values for schedule
-  const setTimeValues = () => {
-    let timeSchedule = [];
-    const minutes = ['00', '15', '30', '45'];
-    let hour = 10;
-    for (let y = 0; y < 12; y++) {
-      const map = minutes.map((m) => `${hour}:${m}`);
-      hour++;
-      timeSchedule = timeSchedule.concat(map);
+arrowPrev.addEventListener('click', () => {
+  const prevMonth = date.getMonth() - 1;
+  date.setMonth(prevMonth);
+  renderCalendar();
+  console.log(date);
+  console.log(prevMonth);
+});
+arrowNext.addEventListener('click', () => {
+  const nextMonth = date.getMonth() + 1;
+  date.setMonth(nextMonth);
+  renderCalendar();
+  console.log(date);
+  console.log(nextMonth);
+});
+
+//setting time values for schedule
+const setTimeValues = () => {
+  let timeSchedule = [];
+  const minutes = ['00', '15', '30', '45'];
+  let hour = 10;
+  for (let y = 0; y < 12; y++) {
+    const map = minutes.map((m) => `${hour}:${m}`);
+    hour++;
+    timeSchedule = timeSchedule.concat(map);
+  }
+  const timeSelect = document.querySelector('#time-select');
+  timeSchedule.forEach((time) => {
+    let option = document.createElement('option');
+    option.innerText = time;
+    option.setAttribute('data-time-index', `${timeSchedule.indexOf(time)}`);
+    option.className = 'time-option';
+    timeSelect.appendChild(option);
+  });
+};
+const timeCheck = () => {
+  const timeOptions = document.querySelectorAll('.time-option');
+  scheduleValuesData.forEach((data) => {
+    if (clickedDateId === data.date) {
+      scheduleTimeData.push(...data.time);
     }
-    const timeSelect = document.querySelector('#time-select');
-    timeSchedule.forEach((time) => {
-      let option = document.createElement('option');
-      option.innerText = time;
-      option.setAttribute('data-time-index', `${timeSchedule.indexOf(time)}`);
-      option.className = 'time-option';
-      timeSelect.appendChild(option);
-    });
-  };
-  const timeCheck = () => {
-    const timeOptions = document.querySelectorAll('.time-option');
-    scheduleValuesData.forEach((data) => {
-      if (clickedDateId === data.date) {
-        scheduleTimeData.push(...data.time);
-      }
-    });
-    timeOptions.forEach((time) => {
-      if (time.classList.contains('hide')) {
-        time.classList.remove('hide');
-        time.disabled = false;
-      }
-      let timeIndexAttribute = time.getAttribute('data-time-index');
-      timeIndexes.push(Number(timeIndexAttribute));
-      if (scheduleTimeData.includes(Number(timeIndexAttribute))) {
-        time.classList.add('hide');
-        time.disabled = true;
-      }
-    });
-    freeScheduleTime = timeIndexes.filter(
-      (elem) => !scheduleTimeData.includes(Number(elem))
-    );
-    checkMassageOptionValidity(freeScheduleTime);
-  };
-  const removeErrorClass = (elem) => {
-    if (elem.value !== '' && elem.classList.contains('error-style')) {
-      elem.classList.remove('error-style');
+  });
+  timeOptions.forEach((time) => {
+    if (time.classList.contains('hide')) {
+      time.classList.remove('hide');
+      time.disabled = false;
     }
-  };
-  const resetScheduleValues = () => {
-    schedulesSelect.forEach((elem) => {
-      elem.value = '';
-      elem.classList.remove('error-style');
-    });
-  };
+    let timeIndexAttribute = time.getAttribute('data-time-index');
+    timeIndexes.push(Number(timeIndexAttribute));
+    if (scheduleTimeData.includes(Number(timeIndexAttribute))) {
+      time.classList.add('hide');
+      time.disabled = true;
+    }
+  });
+  freeScheduleTime = timeIndexes.filter(
+    (elem) => !scheduleTimeData.includes(Number(elem))
+  );
+  checkMassageOptionValidity(freeScheduleTime);
+};
+const removeErrorClass = (elem) => {
+  if (elem.value !== '' && elem.classList.contains('error-style')) {
+    elem.classList.remove('error-style');
+  }
+};
+const resetScheduleValues = () => {
   schedulesSelect.forEach((elem) => {
-    elem.addEventListener('click', () => {
-      if (elem.classList.contains('error-style')) {
-        removeErrorClass(elem);
-      }
-    });
+    elem.value = '';
+    elem.classList.remove('error-style');
   });
-  const massageOptions = document.querySelectorAll('.massage-option');
-  const checkMassageOptionValidity = (freeScheduleTime) => {
-    massageOptions.forEach((opt) => {
-      if (opt.classList.contains('hide')) {
-        opt.classList.remove('hide');
-        opt.disabled = false;
-      }
-      let massageValue = opt.getAttribute('data-massage-value');
-      const result = isContinuous(freeScheduleTime, massageValue);
-      if (!result) {
-        opt.classList.add('hide');
-        opt.disabled = true;
-      }
-    });
-  };
-  const isContinuous = (freeScheduleTime, massageValue) => {
-    for (let i = 0; i < freeScheduleTime.length; i++) {
-      let isSequence = true;
-      for (let j = 1; j < massageValue - 1; j++) {
-        if (freeScheduleTime[i + j] !== freeScheduleTime[i] + j) {
-          isSequence = false;
-          break;
-        }
-      }
-      if (isSequence) {
-        return true;
+};
+schedulesSelect.forEach((elem) => {
+  elem.addEventListener('click', () => {
+    if (elem.classList.contains('error-style')) {
+      removeErrorClass(elem);
+    }
+  });
+});
+const massageOptions = document.querySelectorAll('.massage-option');
+const checkMassageOptionValidity = (freeScheduleTime) => {
+  massageOptions.forEach((opt) => {
+    if (opt.classList.contains('hide')) {
+      opt.classList.remove('hide');
+      opt.disabled = false;
+    }
+    let massageValue = opt.getAttribute('data-massage-value');
+    const result = isContinuous(freeScheduleTime, massageValue);
+    if (!result) {
+      opt.classList.add('hide');
+      opt.disabled = true;
+    }
+  });
+};
+const isContinuous = (freeScheduleTime, massageValue) => {
+  for (let i = 0; i < freeScheduleTime.length; i++) {
+    let isSequence = true;
+    for (let j = 1; j < massageValue - 1; j++) {
+      if (freeScheduleTime[i + j] !== freeScheduleTime[i] + j) {
+        isSequence = false;
+        break;
       }
     }
-    return false;
-  };
-  schBackBtn.addEventListener('click', () => {
-    scheduleTimeData = [];
-    timeIndexes = [];
-    freeScheduleTime = [];
-  });
-  schContinueBtn.addEventListener('click', () => {
-    checkTimeAndMassage();
-    let allSelectsHaveValue = true;
-    schedulesSelect.forEach((elem) => {
-      if (elem.value === '') {
-        elem.classList.add('error-style');
-        scheduleModal.hide();
-        errorModal.show();
-        allSelectsHaveValue = false;
-      } else {
-        allSelectsHaveValue = true;
-        removeErrorClass(elem);
-      }
-    });
-    if (allSelectsHaveValue && checkTime) {
+    if (isSequence) {
+      return true;
+    }
+  }
+  return false;
+};
+schBackBtn.addEventListener('click', () => {
+  scheduleTimeData = [];
+  timeIndexes = [];
+  freeScheduleTime = [];
+});
+schContinueBtn.addEventListener('click', () => {
+  checkTimeAndMassage();
+  let allSelectsHaveValue = true;
+  schedulesSelect.forEach((elem) => {
+    if (elem.value === '') {
+      elem.classList.add('error-style');
       scheduleModal.hide();
-      formModal.show();
-    }
-    if (!checkTime) {
-      alert('Time and Massage not suitable'); //make new modal for this !!!!!!!!!!!!!!!
+      errorModal.show();
+      allSelectsHaveValue = false;
+    } else {
+      allSelectsHaveValue = true;
+      removeErrorClass(elem);
     }
   });
-  const checkTimeAndMassage = () => {
-    checkTime = true;
-    const schMassIndex = massageSelect.selectedIndex;
-    massageDataValue = Number(
-      massageSelect.options[schMassIndex].dataset.massageValue
-    );
-    const schTimeIndex = timeSelect.selectedIndex;
-    timeDataIndex =
-      Number(timeSelect.options[schTimeIndex].dataset.timeIndex) + 1;
-    for (
-      i = timeDataIndex;
-      i < timeDataIndex + massageDataValue - 1 && i < 48;
-      i++
-    ) {
-      let timeClass = timeSelect[i].classList.value;
-      const noSpace = timeClass.replace(/ /g, '');
-      if (noSpace.includes('hide')) {
-        checkTime = false;
-      }
+  if (allSelectsHaveValue && checkTime) {
+    scheduleModal.hide();
+    formModal.show();
+  }
+  if (!checkTime) {
+    alert('Time and Massage not suitable'); //make new modal for this !!!!!!!!!!!!!!!
+  }
+});
+const checkTimeAndMassage = () => {
+  checkTime = true;
+  const schMassIndex = massageSelect.selectedIndex;
+  massageDataValue = Number(
+    massageSelect.options[schMassIndex].dataset.massageValue
+  );
+  const schTimeIndex = timeSelect.selectedIndex;
+  timeDataIndex =
+    Number(timeSelect.options[schTimeIndex].dataset.timeIndex) + 1;
+  for (
+    i = timeDataIndex;
+    i < timeDataIndex + massageDataValue - 1 && i < 48;
+    i++
+  ) {
+    let timeClass = timeSelect[i].classList.value;
+    const noSpace = timeClass.replace(/ /g, '');
+    if (noSpace.includes('hide')) {
+      checkTime = false;
     }
-  };
+  }
 };
